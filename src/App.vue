e750b6f720b240a1557552f317fe8e9ba0ef1021
dineshranga95 <dineshranga570@gmail.com>
2020-05-02 11:12:15 +0530

    file created

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+   
+    <h1>{{ msg }}</h1>
+    <h2>{{ value }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      value: 'vue js music app'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
