9161caae56022d86dd70ac482c56df7dc194cccf
dineshranga95 <dineshranga570@gmail.com>
2020-05-05 17:11:32 +0530

    navbar created

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,13 @@
 <template>
-  <div>
-   
-    <h1>{{ msg }}</h1>
-    <h2>{{ value }}</h2>
-  </div>
+<div>
+  <app-nav></app-nav>
+</div>
 </template>
 
 <script>
+
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-      value: 'vue js music app'
-    }
-  }
+
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import navbar from './components/navbar.vue'
 
+Vue.component('app-nav',navbar),
 new Vue({
   el: '#app',
   render: h => h(App)
