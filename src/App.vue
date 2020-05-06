94c31b72079fbaeaf2d4e4d1f8a5158429243345
dineshranga95 <dineshranga570@gmail.com>
2020-05-06 20:32:45 +0530

    componenets created

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
 <div>
   <app-nav></app-nav>
+  <search-bar></search-bar>
 </div>
 </template>
 
 <script>
-
+import home from './components/home.vue'
 export default {
-
+components:{
+  'search-bar':home
+}
 }
 </script>
 
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,21 +1,30 @@
 <template>
-  <div>
+  <div class="container" >
+  
+   <form class="form-horizontal ml-5 mr-5">
+  <fieldset>
+    <legend>Music app</legend>
+    <div class="form-group row ml-5">
+      <label for="staticEmail" class="col-lg-2 control-label">Search music</label>
+      <div class="col-lg-10">
+        <input type="text"  class="form-control mr-5" id="staticEmail" placeholder="search here">
+      </div>
+    </div>
+    
+ </fieldset>  
+</form>
+    <div class="row mt-2" >
+     <div class="col-md-12" style="padding-left:120px;">
+        Results
+      </div>
    
-    <h1>{{ msg }}</h1>
-    <h2>{{ value }}</h2>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      
-      msg: 'Welcome to Your Vue.js App',
-      value: 'vue js music app'
-    }
-  }
+ 
 }
 </script>
 
